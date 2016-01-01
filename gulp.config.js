d53da243ec5c9125fe63d2083b6885ecaaebdf07
zakharek <ekaterina.zhr@gmail.com>
2016-01-01 20:29:44 +1100

    Add codecheck gulp task

---
+++ gulp.config.js
@@ -0,0 +1,12 @@
+﻿module.exports = function () {
+    var config = {
+        allJs: [
+            './js/**/*.js'//,
+            //'./*.js'
+        ],
+        port: 8000,
+        pageSpeedThreshold: 90
+    };
+
+    return config;
+};
--- gulpfile.js
+++ gulpfile.js
@@ -3,38 +3,47 @@
 //https://github.com/gulpjs/gulp/blob/master/docs/getting-started.md
 //http://www.hongkiat.com/blog/access-localhost-public-address/
 //http://una.im/gulp-local-psi/
-var gulp = require('gulp'),
+var config = require('./gulp.config')(),
+    gulp = require('gulp'),
+    jshint = require('gulp-jshint'),
+    jscs = require('gulp-jscs'),
     connect = require('gulp-connect'),
     ngrok = require('ngrok'), //https://github.com/bubenshchykov/ngrok/issues/34#issuecomment-155420006
     psi = require('psi'),
     sequence = require('run-sequence'),
-    PORT = 8000,
-    PAGE_SPEED_THRESHOLD = 90,
     site,
     desktopPsiData,
     mobilePsiData;
 
+gulp.task('codecheck', function () {
+    return gulp
+    .src(config.allJs)
+   // .pipe(jscs({ fix: false }));
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish', { verbose: true }));
+});
+
 function runPsi(strategy, doneCallback) {
     console.log("running psi for strategy " + strategy);
-    console.log("target psi threshold is " + PAGE_SPEED_THRESHOLD);
+    console.log("target psi threshold is " + config.pageSpeedThreshold);
 
     psi(site, {
         nokey: 'true',
         strategy: strategy,
-        threshold: PAGE_SPEED_THRESHOLD
+        threshold: config.pageSpeedThreshold
     }).then(doneCallback);
 }
 
 gulp.task('connect', function () {
     connect.server({
-        port: PORT
+        port: config.port
     });
 
-    console.log('Started local server: http://localhost:' + PORT);
+    console.log('Started local server: http://localhost:' + config.port);
 });
 
 gulp.task('ngrok', function (cb) {
-    return ngrok.connect({ port: PORT, nokey: true }, function (err, url) {
+    return ngrok.connect({ port: config.port, nokey: true }, function (err, url) {
         console.log('started ngrok tunnel: ' + url);
         site = url;
         cb(err);
@@ -68,15 +77,20 @@ gulp.task('psi-seq', function (cb) {
 gulp.task('psi', ['psi-seq'], function () {
 
     var desktopSpeed = desktopPsiData.ruleGroups.SPEED.score,
-        desktopSpeedIsOk = desktopSpeed >= PAGE_SPEED_THRESHOLD,
+        desktopSpeedIsOk = desktopSpeed >= config.pageSpeedThreshold,
         mobileSpeed = mobilePsiData.ruleGroups.SPEED.score,
-        mobileSpeedIsOk = mobileSpeed >= PAGE_SPEED_THRESHOLD;
+        mobileSpeedIsOk = mobileSpeed >= config.pageSpeedThreshold;
 
-    console.log("desktop page speed " + desktopSpeed + " does" + (desktopSpeedIsOk ? "" : " not") + " meet threshold " + PAGE_SPEED_THRESHOLD);
-    console.log("mobile page speed " + mobileSpeed + " does" + (mobileSpeedIsOk ? "" : " not") + " meet threshold " + PAGE_SPEED_THRESHOLD);
+    console.log("desktop page speed " + desktopSpeed + " does" + (desktopSpeedIsOk ? "" : " not") + " meet threshold " + config.pageSpeedThreshold);
+    console.log("mobile page speed " + mobileSpeed + " does" + (mobileSpeedIsOk ? "" : " not") + " meet threshold " + config.pageSpeedThreshold);
 
+    console.log("Stats for desktop:");
     console.log(desktopPsiData.pageStats);
     console.log(JSON.stringify(desktopPsiData, null, 4));
+
+    console.log("Stats for mobile:");
+    console.log(mobilePsiData.pageStats);
+    console.log(JSON.stringify(mobilePsiData, null, 4));
 
     process.exit(desktopSpeedIsOk && mobileSpeedIsOk ? 0 : 1);
 });
